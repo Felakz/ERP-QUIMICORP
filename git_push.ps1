bfb4f85b93432d013825385e1878e80910061594
Felakz <dczl0603@gmail.com>
2026-08-08 13:29:19 -0500

    feat(admin-sidebar): estructura de categorias, nuevas pestanas clientes/facturacion/cobranzas/inventario/alertas-stock

---
+++ git_push.ps1
@@ -0,0 +1,5 @@
+$git = 'C:\Users\Lenovo\AppData\Local\GitHubDesktop\app-3.6.3\resources\app\git\cmd\git.exe'
+& $git add .
+& $git status --short
+& $git commit -m 'feat(admin-sidebar): estructura de categorias, nuevas pestanas clientes/facturacion/cobranzas/inventario/alertas-stock'
+& $git push origin feature/Desarrollo
