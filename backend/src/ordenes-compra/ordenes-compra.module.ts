588a901f174c39bbf2eebd414c7df28ac9023e9d
Sistemas-FLK <sistemas@grupoquimicorp.pe>
2026-09-17 12:08:36 -0500

    fix(backend): importar PrismaModule y AuthModule en OrdenesCompraModule

--- backend/src/ordenes-compra/ordenes-compra.module.ts
+++ backend/src/ordenes-compra/ordenes-compra.module.ts
@@ -1,8 +1,11 @@
 import { Module } from '@nestjs/common';
 import { OrdenesCompraService } from './ordenes-compra.service';
 import { OrdenesCompraController } from './ordenes-compra.controller';
+import { PrismaModule } from '../common/prisma/prisma.module';
+import { AuthModule } from '../auth/auth.module';
 
 @Module({
+  imports: [PrismaModule, AuthModule],
   controllers: [OrdenesCompraController],
   providers: [OrdenesCompraService],
 })
